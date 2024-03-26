28a1af6dce3a0e4aae36da39ecb962fedf4fff1a
Aryan9626 <155606773+Aryan9626@users.noreply.github.com>
2024-03-26 17:36:57 +0530

    C-04: Update HelloJenkins.java

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -2,5 +2,6 @@ public class HelloJenkins {
   public static void main(String[] args) {
     System.out.println("Hello, Jenkins");
     System.out.println("Edit-1");
+    System.out.println("Edit-2");
   }
 }
